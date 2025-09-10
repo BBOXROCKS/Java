0ff1639cf54d184ea61397334a8abdff8dcf1f7b
BBOXROCKS <137446942+BBOXROCKS@users.noreply.github.com>
2025-09-10 21:58:45 +0000

    Update with new files

---
+++ Solutions/Assignment2Solution.java
@@ -0,0 +1,12 @@
+//Create a project to find the average of 3 numbers
+
+public class Assignment2Solution {
+    public static void main(String[] args) {
+        double num1 = 2.1;
+        double num2 = 7.9;
+        double num3 = 11.3;
+
+        double average = (num1 + num2 + num3)/3;
+        System.out.println("The average is: " + average);
+    }
+}
